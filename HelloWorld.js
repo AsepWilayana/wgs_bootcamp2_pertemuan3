59ebe1b15e7da50edcf8fffabf3555a6f6c5833e
AsepWilayana <asepwilayana@gmail.com>
2022-07-06 09:52:23 +0700

    data diri

---
+++ HelloWorld.js
@@ -0,0 +1,7 @@
+const nama = 'Asep Wilayana';
+const ttl = 'Bandung, 13 juli 1996';
+const alamat = 'Jl. babakan Tarogong';
+
+console.info(`Nama : ${nama}`);
+console.info(`Tempat, Tanggal Lahir : ${ttl}`);
+console.info(`Alamat : ${alamat}`);
